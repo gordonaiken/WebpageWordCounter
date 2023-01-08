9fb02a016e83a65eac905ee1a6b75575fa5a79b2
G <aiken.gordon@gmail.com>
2023-01-08 13:33:08 +0000

    Unspecified number of URLs
    Adapted the code to take an unspecified number of URLs from the user

--- WebsiteWordCounter/main.py
+++ WebsiteWordCounter/main.py
@@ -20,39 +20,32 @@ def count_words(url):
     return word_counts
 
 # Ask the user for the URLs of the webpages
-#url1 = input("Enter the URL of the first webpage: ")
-#url2 = input("Enter the URL of the second webpage: ")
-#url3 = input("Enter the URL of the third webpage: ")
-url1 = 'https://www.itecco.co.uk/job/graduate-software-engineer-6?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic'
-url2 = 'https://www.brightnetwork.co.uk/graduate-jobs/starling-bank/graduate-software-engineer-london-2023?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic'
-url3 = 'https://www.totaljobs.com/job/graduate-software-developer/spectrum-it-recruitment-south-ltd-job99529127?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic'
-
+urls = []
+url = input("Enter the URL of a webpage (enter 'done' when finished): ")
+while url != 'done':
+    urls.append(url)
+    url = input("Enter the URL of a webpage (enter 'done' when finished): ")
 
 # Count the words on each webpage
-word_counts1 = count_words(url1)
-word_counts2 = count_words(url2)
-word_counts3 = count_words(url3)
+word_counts_list = []
+for url in urls:
+    word_counts = count_words(url)
+    word_counts_list.append(word_counts)
 
 # Create a table to display the results
 table = PrettyTable()
 table.field_names = ["Word", "Frequency"]
 
-# Add the results for the first webpage to the table
-for word, count in word_counts1.items():
-    table.add_row([word, count])
-
-# Add the results for the second webpage to the table
-for word, count in word_counts2.items():
-    table.add_row([word, count])
-
-# Add the results for the third webpage to the table
-for word, count in word_counts3.items():
-    table.add_row([word, count])
+# Add the results for each webpage to the table
+for word_counts in word_counts_list:
+    for word, count in word_counts.items():
+        table.add_row([word, count])
 
 # Display the table
 table.sortby = "Frequency"
 table.reversesort = True
 print(table)
+
 
 
 
